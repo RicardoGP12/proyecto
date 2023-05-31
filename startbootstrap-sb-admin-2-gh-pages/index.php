423000df8b7082323c546caa31ae7b24b7664338
VanessaGL7 <glvanessa2@gmail.com>
2023-05-30 23:32:18 -0600

    Modificacion1

--- startbootstrap-sb-admin-2-gh-pages/index.php
+++ startbootstrap-sb-admin-2-gh-pages/index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>CoffeSmart - Panel de administración</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
